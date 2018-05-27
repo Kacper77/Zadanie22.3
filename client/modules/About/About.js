546d69edac5f6560f29bb96720deaf896ef6c076
Kacper Stroisz <kacperstroisz@wp.pl>
2018-05-27 13:14:36 +0200

    Zadanie 22.6

---
+++ client/modules/About/About.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class About extends Component {
+  render() {
+    return ( <
+      div >
+      <
+      h2 > About < /h2> < /
+      div >
+    );
+  }
+}
+
+export default About;
